2de84279415a36b156fbfabaab82b92ffd84aa54
Rana_Lao <149959793+RanaG6@users.noreply.github.com>
2025-06-21 03:19:28 +0700

    app.py

--- PP_Y3S2_Movie/app.py
+++ PP_Y3S2_Movie/app.py
@@ -1,10 +1,4 @@
 import streamlit as st
-import pickle
-
-movies = pickle.load(open("movies_list.pkl", 'rb'))
 
 st.header("Movie Recommender System")
-st.selectbox("Select mvoie from dropdown", movies)
 
-if st.button("Show Recommend");
-    pass
